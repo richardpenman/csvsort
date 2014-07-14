ff5f50271cd9d7a63738ac3199919417a7762235
Richard Penman <26063146+richardpenman@users.noreply.github.com>
2014-07-15 01:06:10 +0800

    initial commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+import os
+from distutils.core import setup
+
+def read(filename):
+    return open(os.path.join(os.path.dirname(__file__), filename)).read()
+
+setup(
+    name='csvsort', 
+    version='1.0',
+    packages=['csvsort'],
+    package_dir={'csvsort' : '.'}, 
+    author='Richard Penman',
+    author_email='richard@webscraping.com',
+    description='Sort large CSV files on disk rather than in memory',
+    long_description=read('README.rst'),
+    url='https://bitbucket.org/richardpenman/csvsort',
+    license='lgpl',
+)
